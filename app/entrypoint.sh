a6df8fa24b9363592824df11b327965fa3ad1f4d
swelanaugsute <swelanauguste@gmail.com>
2024-05-16 15:43:01 -0400

    postgres

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,22 +1,21 @@
 #!/bin/sh
 
-# if [ "$DATABASE" = "postgres" ]
-# then
-#     echo "Waiting for postgres..."
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
 
-#     while ! nc -z $SQL_HOST $SQL_PORT; do
-#       sleep 0.1
-#     done
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
 
-#     echo "PostgreSQL started"
-# fi
+    echo "PostgreSQL started"
+fi
 
-# python manage.py makemigrations --merge
 python manage.py migrate
 python manage.py makemigrations
 python manage.py migrate
-python manage.py makemigrations
-python manage.py createsuperuser --username cworks --email ict.infrastructure@govt.lc --no-input
-python manage.py collectstatic --no-input
+python manage.py createsuperuser --username cworks --email ict.infrastructure@govt.lc --noinput
+# python manage.py loaddata  --exclude=auth.permission --exclude=contenttypes db3_5_2024.json
+python manage.py collectstatic --noinput
 
 exec "$@"
